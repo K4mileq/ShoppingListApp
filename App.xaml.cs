1b3d4da1eef9bd715c84d5aab1d098a8df681cae
K4mileq <kamilc2006@gmail.com>
2025-01-13 17:55:32 +0100

    Add files via upload

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,16 @@
-﻿
-namespace ShopingListApp
+﻿namespace ShopingListApp
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
+            // Ładowanie stylów CSS
+            Resources.Add(new StyleSheet
+            {
+                Source = new Uri("Resources/Styles/styles.css", UriKind.Relative)
+            });
 
             MainPage = new AppShell();
         }
