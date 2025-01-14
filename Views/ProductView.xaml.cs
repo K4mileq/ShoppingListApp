88196a1a9dbbf9940b43439942e06599f76819e2
K4mileq <kamilc2006@gmail.com>
2025-01-14 06:29:01 +0100

    Update ProductView.xaml.cs

--- Views/ProductView.xaml.cs
+++ Views/ProductView.xaml.cs
@@ -11,41 +11,41 @@ namespace ShopingListApp.Views
             InitializeComponent();
         }
 
-        // Metoda zmniejszaj¹ca iloœæ produktu
+        /
         private void OnDecreaseQuantityClicked(object sender, EventArgs e)
         {
             if (BindingContext is Product product)
             {
                 if (product.Quantity > 0)
                 {
-                    product.Quantity--;  // Zmniejszamy iloœæ
-                    ShoppingListViewModel.Instance.SaveToXml();  // Zapisujemy dane do pliku
+                    product.Quantity--;  
+                    ShoppingListViewModel.Instance.SaveToXml();  
                 }
             }
         }
 
-        // Metoda zwiêkszaj¹ca iloœæ produktu
+        
         private void OnIncreaseQuantityClicked(object sender, EventArgs e)
         {
             if (BindingContext is Product product)
             {
-                product.Quantity++;  // Zwiêkszamy iloœæ
-                ShoppingListViewModel.Instance.SaveToXml();  // Zapisujemy dane do pliku
+                product.Quantity++; 
+                ShoppingListViewModel.Instance.SaveToXml();  
             }
         }
 
-        // Metoda usuwaj¹ca produkt
+       
         private void OnRemoveProductClicked(object sender, EventArgs e)
         {
             if (BindingContext is Product product)
             {
                 var category = ShoppingListViewModel.Instance.Categories
-                    .FirstOrDefault(c => c.Products.Contains(product));  // Znajdujemy kategoriê, do której nale¿y produkt
+                    .FirstOrDefault(c => c.Products.Contains(product));  
 
                 if (category != null)
                 {
-                    category.Products.Remove(product);  // Usuwamy produkt z kategorii
-                    ShoppingListViewModel.Instance.SaveToXml();  // Zapisujemy dane do pliku
+                    category.Products.Remove(product);  
+                    ShoppingListViewModel.Instance.SaveToXml();  
                 }
             }
         }
